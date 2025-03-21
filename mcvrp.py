fd868768456225a8b39dbc1ab4beaf704e5e0cf6
hidek3 <48801339+hidek3@users.noreply.github.com>
2025-03-22 01:16:36 +0900

    結果表現１４

--- mcvrp.py
+++ mcvrp.py
@@ -587,10 +587,10 @@ if st.session_state['best_tour'] !=None:
   result_df=pd.DataFrame({"ノードNo.":node_no,"配送拠点":base_list,"必要物資量":weight_list,"走行距離":distance_list,"巡回順":node_list})
   columnConfig={
                 "ノードNo.": st.column_config.Column(width=25),
-                "配送拠点":  st.column_config.TextColumn(width=100),
-                "必要物資量": st.column_config.TextColumn(width=30),
-                "走行距離": st.column_config.TextColumn(width=30),
-                "巡回順": st.column_config.TextColumn(width=150) 
+                "配送拠点":  st.column_config.Column(width=100),
+                "必要物資量": st.column_config.Column(width=30),
+                "走行距離": st.column_config.Column(width=30),
+                "巡回順": st.column_config.Column(width=150) 
   }
   gis_st.dataframe(result_df,
                column_config = columnConfig
